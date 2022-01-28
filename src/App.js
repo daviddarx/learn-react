0665a6a98b413c36976c1fd5996a3382a6f65451
David Darx <david.darx@liip.ch>
2022-01-28 15:12:12 +0100

    feat(app) create and instanciante sub-components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+const Pet = () => {
+    return React.createElement(
+        "div", 
+        {}, 
+        [
+            React.createElement("h2", {}, "Luna"), 
+            React.createElement("h3", {}, "Dog"), 
+            React.createElement("h3", {}, "Havanese")
+        ]
+    );
+};
+
 const App = () => {
     return React.createElement(
         "div", 
         {
             id: "app-container"
         }, 
-        React.createElement(
-            "h1", 
-            {
-                id: "app-title"
-            }, 
-            "Adopt me"
-        )
+        [
+            React.createElement(
+                "h1", 
+                {
+                    id: "app-title"
+                }, 
+                "Adopt me"
+            ),
+            React.createElement(Pet), 
+            React.createElement(Pet),
+            React.createElement(Pet)
+        ]
+        
     );
 };
 
