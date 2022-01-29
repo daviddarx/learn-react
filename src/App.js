fa474d42b780398f0477c260905455bd2244e7b5
David Darx <david.darx@liip.ch>
2022-01-29 10:25:45 +0100

    feat(search) adds new component with hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams.js";
 
 const App = () => {
     return (
         <div>
             <h1>Adopt me</h1>
-            <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
-            <Pet name="Peeper" animal="Bird" breed="Cockatiel"></Pet>
-            <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier"></Pet>
+            <SearchParams />
         </div>
     );
 };
