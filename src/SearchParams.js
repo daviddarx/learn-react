ba505ce0b58e62ef3093326abfe3226ae43a81de
David Darx <david.darx@liip.ch>
2022-01-29 12:24:27 +0100

    feat(search) adds useEffect
    to get the list of animals on the page load and update the component once it's loaded

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
-const ANIMALS = ["Bird", "Dog", "Cat", "Rabbit"];
+const ANIMALS = ["dog", "rabbit", "bird", "reptile"];
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("Zürich ZH");
+    const [location, setLocation] = useState("");
     const [animal, setAnimal] = useState("");
     const [breed, setBreed] = useState("");
+    const [pets, setPets] = useState([]);
     const breeds = [];
+
+    useEffect(() => {
+        requestPets();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    async function requestPets() {
+        const response = await fetch(
+            `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+        );
+
+        const json = await response.json();
+
+        setPets(json.pets);
+    }
 
     return (
         <div className="search-params">
@@ -61,6 +77,15 @@ const SearchParams = () => {
 
                 <button>Submit</button>
             </form>
+
+            {pets.map((pet) => (
+                <Pet
+                    name={pet.name}
+                    animal={pet.animal}
+                    breed={pet.breed}
+                    key={pet.id}
+                />
+            ))}
         </div>
     );
 };
