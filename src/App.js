d85b70f243be7e9238949a8fa1772641ec15ced0
David Darx <david.darx@liip.ch>
2022-01-30 10:31:53 +0100

    feat(app) adds default routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import SearchParams from "./components/SearchParams.js";
+import Details from "./components/Details.js";
 
 const App = () => {
     return (
         <div>
             <h1>Adopt me</h1>
-            <SearchParams />
+
+            <Router>
+                <Route path="/details/:id">
+                    <Details />
+                </Route>
+                <Route path="/">
+                    <SearchParams />
+                </Route>
+            </Router>
         </div>
     );
 };
---
+++ src/components/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+    return <h2 className="detail">TEst du modle</h2>;
+};
+
+export default Details;
