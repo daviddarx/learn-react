c58826f116fb5c210b12a151d59c36f9b110b4cf
David Darx <david.darx@liip.ch>
2022-01-30 11:16:14 +0100

    feat(detail) adds route informations, load corresponding datas

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,9 +2,33 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 class Details extends Component {
+    constructor() {
+        super();
+
+        this.state = { loading: true };
+    }
+
+    async componentDidMount() {
+        const response = await fetch(
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+        );
+
+        const json = await response.json();
+
+        this.setState(
+            Object.assign(
+                {
+                    loading: false
+                },
+                json.pets[0]
+            )
+        );
+    }
+
     render() {
-        return <h1>test</h1>;
+        console.log(this.state);
+        return <h2>test</h2>;
     }
 }
 
-export default Details;
+export default withRouter(Details);
