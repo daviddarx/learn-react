5e35b1540a1a4c6ed67a3fb567412b8096554f82
David Darx <david.darx@liip.ch>
2022-01-30 10:39:19 +0100

    feat(router) adds switch to the routes
    to display only one match to the complete route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchParams from "./components/SearchParams.js";
 import Details from "./components/Details.js";
 
@@ -8,14 +8,15 @@ const App = () => {
     return (
         <div>
             <h1>Adopt me</h1>
-
             <Router>
-                <Route path="/details/:id">
-                    <Details />
-                </Route>
-                <Route path="/">
-                    <SearchParams />
-                </Route>
+                <Switch>
+                    <Route path="/details/:id">
+                        <Details />
+                    </Route>
+                    <Route path="/">
+                        <SearchParams />
+                    </Route>
+                </Switch>
             </Router>
         </div>
     );
