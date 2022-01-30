285b3ab432fcae2d489d2c142a93f6541c82c9aa
David Darx <david.darx@liip.ch>
2022-01-30 10:52:21 +0100

    feat(router) implements router links instead of html links
    to avoid page reload

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Pet = ({ name, animal, breed, images, city, state, id }) => {
     let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
 
@@ -8,7 +10,7 @@ const Pet = ({ name, animal, breed, images, city, state, id }) => {
     const location = `${city}, ${state}`;
 
     return (
-        <a href={`/details/${id}`} className="pet">
+        <Link to={`/details/${id}`} className="pet">
             <div className="image-container">
                 <img src={hero} alt={name} />
             </div>
@@ -16,7 +18,7 @@ const Pet = ({ name, animal, breed, images, city, state, id }) => {
                 <h1>{name}</h1>
                 <h2>{`${animal} - ${breed} - ${location}`}</h2>
             </div>
-        </a>
+        </Link>
     );
 };
 
