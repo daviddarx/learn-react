6a6807c752a07b243a7fcf67017055e9605eb44c
David Darx <david.darx@liip.ch>
2022-01-30 10:52:52 +0100

    feat(detail) make a class component instead of function component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,10 @@
-const Details = () => {
-    return <h2 className="detail">TEst du modle</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+    render() {
+        return <h1>test</h1>;
+    }
+}
 
 export default Details;
